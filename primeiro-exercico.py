6290455bdddb22146564daa2ca03e90c9ac246b7
jose-alexx <josealex41012@gmail.com>
2024-08-04 08:23:06 -0300

    comando-de-entrada

--- primeiro-exercico.py
+++ primeiro-exercico.py
@@ -4,6 +4,16 @@ mensagem = 'Exemplo de mensagem!'
 n = 25
 pi = 3.141592653589931
 
-print(mensagem); # Exemplo de mensagem!
-print(n); # 25
-print(pi); #3.141592653589931
+print(mensagem) # Exemplo de mensagem!
+print(n) # 25
+print(pi) #3.141592653589931
+
+# Entrada
+
+nome = input("Informe o se nome: ")
+idade = int(input("Informa a sua idade: "))
+nota = float(input("Informa a sua primeira nota: "))
+
+print("Seu nome é: ", nome)
+print("Sua idade é: ", idade)
+print("Sua nota é: ", nota)
