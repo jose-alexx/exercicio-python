12db2b317786887017346723073ddb81bc14700b
jose-alexx <josealex41012@gmail.com>
2024-08-04 08:30:39 -0300

    operacoes

--- calculadora.py
+++ calculadora.py
@@ -5,3 +5,24 @@ peso = float(input("Digite seu peso em quilogramas: "))
              
 imc = peso / (altura ** 2)
 print("Seu IMC é: ", imc)
+
+# Operações aritimetricas
+
+numero__1 = int(input("Digite o primeiro numero: ")) # 5
+numero__2 = int(input("Digite o segundo numero: ")) # 2
+
+soma = numero_1 + numero_2
+subtracao = numero_1 - numero_2
+multiplicacao = numero_1 * numero_2
+divisao = numero_1 / numero_2
+divisao_inteira = numero1 // numero_2
+modulo =numero_1 % numero_2
+exponenciacao = numero_1 ** numero_2
+
+print(soma) # 7
+print(subtracao) # 3
+print(multiplicacao) # 10
+print(divisao) # 2.5
+print(divisao__inteira) # 2
+print(modulo) # 1
+print(exponenciacao) # 25
